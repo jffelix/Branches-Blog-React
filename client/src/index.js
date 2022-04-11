696f3983a58fb6b4876a6136938b79847f5c8f45
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-04-11 12:46:48 -0700

    Removed ReactDOM for createRoot in src/index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 
 const rootElement = document.getElementById("app");
+const root = createRoot(rootElement);
 
-ReactDOM.render(<App />, rootElement);
+root.render(<App />);
