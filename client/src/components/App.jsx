6f0d4ddf26094323851c01ef73249b75c709fc19
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-04-12 00:12:00 -0700

    Removed Switch for Routes in react-router-dom

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,17 +3,23 @@ import SignIn from "./login/SignIn.jsx";
 import SignUp from "./login/SignUp.jsx";
 import { Container } from "react-bootstrap";
 import AuthProvider from "../context/authContext.js";
-import { BrowserRouter as Router, Swtich, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
 
     return (
         <div>
-            <AuthProvider>
-                <Container>
-                    <SignUp />
-                </Container>
-            </AuthProvider>
+            <Container>
+            <div>
+                <Router>
+                    <AuthProvider>
+                        <Routes>
+                            <Route path="/" element={<SignUp />} />
+                        </Routes>
+                    </AuthProvider>
+                </Router>
+            </div>
+            </Container>
         </div>
     )
 }
