611bfc185153515084e27d6eeee32c63d68f5d7c
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-04-12 23:38:58 -0700

    Added controllers/index.js

---
+++ server/controllers/index.js
@@ -0,0 +1,22 @@
+const User = require("../database/user.js");
+
+const signUp = async (req, res) => {
+    const { name, email, userId } = req.body;
+
+    try {
+      const user = await User.find({email});
+
+      if (!user.length) {
+        const user = await User.create({ name, email, userId });
+        return res.status(201).send({message: 'New user created.'});
+      }
+      return res.sendStatus(200);
+
+    } catch (error) {
+      res.status(400).send(error);
+    }
+};
+
+module.exports = {
+    signUp
+}
