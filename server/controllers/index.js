428bc3f4b41f046d2058a1ae8f74311967507105
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-04-13 17:28:50 -0700

    Connected axios POST request to MongoDB database

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -7,21 +7,18 @@ const signUp = async (req, res) => {
     // create database
       // add user to database
 
+    try {
+      const user = await User.find({email});
 
+      if (!user.length) {
+        const user = await User.create({ username, email });
+        return res.status(201).send('New user created.');
+      }
+      return res.sendStatus(200);
 
-
-    // try {
-    //   const user = await User.find({email});
-
-    //   if (!user.length) {
-    //     const user = await User.create({ name, email });
-    //     return res.status(201).send('New user created.');
-    //   }
-    //   return res.sendStatus(200);
-
-    // } catch (error) {
-    //   res.status(400).send(error);
-    // }
+    } catch (error) {
+      res.status(400).send(error);
+    }
 };
 
 module.exports = {
