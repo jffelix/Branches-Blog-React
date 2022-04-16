11b0db7278213631bc26228d4894e12e4ebda92d
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-04-15 18:59:55 -0700

    Refactored deleteComment to also use Post.find()

--- server/controllers/comment.js
+++ server/controllers/comment.js
@@ -30,30 +30,26 @@ const addComment = async (req, res) => {
 const deleteComment = async (req, res) => {
     const commentId = req.params.id;
     const postId = req.body.postId;
-    console.log("commentId: ", commentId);
-    console.log("postId: ", postId);
-
+    // console.log("commentId: ", commentId);
+    // console.log("postId: ", postId);
     try {
         // remember the comment is in a nested array
           // with Post, it will only reveal the post, not the comment
-        const post = await Post.findOneAndUpdate({
-            _id: postId
-        }, {
-            $pull: {
-                comments: {
-                    _id: commentId
+        const post = await Post.find({_id: postId})
+
+        if (post.length === 1) {
+            const deletePost = await Post.findOneAndUpdate({
+                _id: postId
+            }, {
+                $pull: {
+                    comments: {
+                        _id: commentId
+                    }
                 }
-            }
-        });
-
-        console.log("post: ", post);
-
-        // if (post.length === 1) {
-        //     // console.log("post: ", post);
-        //     const deleteComment = await Post.findOneAndDelete({_id: commentId});
-        //     return res.status(200).send("Post deleted!");
-        // }
-        // return res.status(400).send("Error while deleting post.");
+            });
+            return res.status(200).send("Post deleted!");
+        }
+        return res.status(400).send("Error while deleting post.");
 
     } catch {
         return res.status(400).send("Error while deleting post.");
