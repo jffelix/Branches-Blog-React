7bb1ce0d71db82a8888b1ee0b678304b35e5a204
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-04-17 11:30:09 -0700

    Created redirect links for ForgotPassword and MyProfile

--- client/src/components/MyProfile.jsx
+++ client/src/components/MyProfile.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const MyProfile = () => {
+
+    const backToDashboard = () => {
+        navigate("/dashboard", { replace: true });
+    }
 
     return (
         <div>
             <h3>Hello from MyProfile!</h3>
+            <button onClick={backToDashboard}>Back to Dashboard</button>
         </div>
     )
 }
--- client/src/components/login/ForgotPassword.jsx
+++ client/src/components/login/ForgotPassword.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const ForgotPassword = () => {
+
+    const navigate = useNavigate();
+
+    const backToSignUp = () => {
+
+        navigate("/", { replace: true });
+    }
+
 
     return (
         <div>
             <h3>Hello from ForgotPassword Component!</h3>
+            <button onClick={backToSignUp}>Back to Sign Up</button>
         </div>
     )
 }
