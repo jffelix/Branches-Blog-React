31b7092106c7e7e5181f7948cc8edf66e39ac7fb
Jean-Luc Felix <jlfelix522@yahoo.com>
2022-04-22 14:24:19 -0700

    Created basic template of ForgotPassword with additional link

--- client/src/components/login/ForgotPassword.jsx
+++ client/src/components/login/ForgotPassword.jsx
@@ -1,22 +1,75 @@
-import React from "react";
+import React, { useState, useRef } from "react";
+import { Form, Button, Card, Alert } from "react-bootstrap";
+import { useAuth } from "../../context/authContext.js";
 import { Link, useNavigate } from "react-router-dom";
 
 const ForgotPassword = () => {
 
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const { signIn } = useAuth();
+    const [ error, setError ] = useState("");
+    const [ loading, setLoading ] = useState(false);
     const navigate = useNavigate();
 
-    const backToSignUp = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
-        navigate("/", { replace: true });
-    }
-
+        try {
+            setError("");
+            setLoading(true);
+            await signIn(emailRef.current.value, passwordRef.current.value);
+            navigate("/dashboard", { replace: true });
+        } catch {
+            setError("Failed to sign in");
+        }
+        setLoading(false);
+    } 
 
     return (
-        <div>
-            <h3>Page under construction. Please return to Sign Up</h3>
-            <button onClick={backToSignUp}>Back to Sign Up</button>
+        <div className="fullForgotPassword">
+            <div className="forgotPassword">
+                <h2>Log In</h2>
+                {error && <Alert variant="danger">{error}</Alert>}
+                <form onSubmit={(e) => handleSubmit(e)}>
+                    <p>Email</p>
+                    <input type="email" ref={emailRef} required />
+                    <p></p>
+                    <button>Reset Password</button>
+                </form>
+                <div>
+                    <Link to="/signin">Log In</Link>
+                </div>
+                <div>
+                    Don't have an account? <Link to="/">Sign Up</Link>
+                </div>
+            </div>
         </div>
     )
 }
 
 export default ForgotPassword;
+
+
+
+// import React from "react";
+// import { Link, useNavigate } from "react-router-dom";
+
+// const ForgotPassword = () => {
+
+//     const navigate = useNavigate();
+
+//     const backToSignUp = () => {
+//         navigate("/", { replace: true });
+//     }
+
+
+//     return (
+//         <div>
+//             <h3>Page under construction. Please return to Sign Up</h3>
+//             <button onClick={backToSignUp}>Back to Sign Up</button>
+//         </div>
+//     )
+// }
+
+// export default ForgotPassword;
